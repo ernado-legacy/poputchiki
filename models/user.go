a1d05eaa750e8e5b74326b917e2fe957160f7985
ernado <ernado@ya.ru>
2014-07-10 19:39:42 +0400

    added likings

--- models/user.go
+++ models/user.go
@@ -13,6 +13,7 @@ import (
 
 var UserWritableFields = []string{"name", "email", "phone", "avatar", "birthday", "seasons",
 	"city", "country", "weight", "growth", "destinations", "sex", "is_sponsor", "is_host",
+	"likings_sex", "likings_destinations", "likings_seasons"
 }
 
 const (
@@ -36,35 +37,38 @@ type Guest struct {
 }
 
 type User struct {
-	Id             bson.ObjectId   `json:"id"                     bson:"_id,omitempty"`
-	Name           string          `json:"name,omitempty"         bson:"name,omitempty"`
-	Sex            string          `json:"sex,omitempty"          bson:"sex,omitempty"`
-	Email          string          `json:"email,omitempty"        bson:"email,omitempty"`
-	EmailConfirmed bool            `json:"email_confirmed"        bson:"email_confirmed"`
-	Phone          string          `json:"phone,omitempty"        bson:"phone,omitempty"`
-	PhoneConfirmed bool            `json:"phone_confirmed"        bson:"phone_confirmed"`
-	IsSponsor      bool            `json:"is_sponsor"             bson:"is_sponsor"`
-	IsHost         bool            `json:"is_host"                bson:"is_host"`
-	Password       string          `json:"-"                      bson:"password"`
-	Online         bool            `json:"online,omitempty"       bson:"online,omitempty"`
-	AvatarUrl      string          `json:"avatar_url,omitempty"   bson:"-"`
-	Avatar         bson.ObjectId   `json:"avatar,omitempty"       bson:"avatar,omitempty"`
-	AvatarWebp     string          `json:"-"                      bson:"image_webp,omitempty"`
-	AvatarJpeg     string          `json:"-"                      bson:"image_jpeg,omitempty"`
-	Balance        uint            `json:"balance,omitempty"      bson:"balance,omitempty"`
-	Age            int             `json:"age,omitempty"          bson:"-"`
-	Birthday       time.Time       `json:"birthday,omitempty"     bson:"birthday,omitempty"`
-	City           string          `json:"city,omitempty"         bson:"city,omitempty"`
-	Country        string          `json:"country,omitempty"      bson:"country,omitempty"`
-	Weight         uint            `json:"weight,omitempty"       bson:"weight,omitempty"`
-	Growth         uint            `json:"growth,omitempty"       bson:"growth,omitempty"`
-	Destinations   []string        `json:"destinations,omitempty" bson:"destinations,omitempty"`
-	Seasons        []string        `json:"seasons,omitempty"      bson:"seasons,omitempty"`
-	LastAction     time.Time       `json:"last_action,omitempty"  bson:"lastaction,omitempty"`
-	StatusUpdate   time.Time       `json:"-"                      bson:"statusupdate,omitempty"`
-	Favorites      []bson.ObjectId `json:"favorites,omitempty"    bson:"favorites,omitempty"`
-	Blacklist      []bson.ObjectId `json:"blacklist,omitempty"    bson:"blacklist,omitempty"`
-	Countries      []string        `json:"countries,omitempty"    bson:"countries,omitempty"`
+	Id                  bson.ObjectId   `json:"id"                     bson:"_id,omitempty"`
+	Name                string          `json:"name,omitempty"         bson:"name,omitempty"`
+	Sex                 string          `json:"sex,omitempty"          bson:"sex,omitempty"`
+	Email               string          `json:"email,omitempty"        bson:"email,omitempty"`
+	EmailConfirmed      bool            `json:"email_confirmed"        bson:"email_confirmed"`
+	Phone               string          `json:"phone,omitempty"        bson:"phone,omitempty"`
+	PhoneConfirmed      bool            `json:"phone_confirmed"        bson:"phone_confirmed"`
+	IsSponsor           bool            `json:"is_sponsor"             bson:"is_sponsor"`
+	IsHost              bool            `json:"is_host"                bson:"is_host"`
+	Password            string          `json:"-"                      bson:"password"`
+	Online              bool            `json:"online,omitempty"       bson:"online,omitempty"`
+	AvatarUrl           string          `json:"avatar_url,omitempty"   bson:"-"`
+	Avatar              bson.ObjectId   `json:"avatar,omitempty"       bson:"avatar,omitempty"`
+	AvatarWebp          string          `json:"-"                      bson:"image_webp,omitempty"`
+	AvatarJpeg          string          `json:"-"                      bson:"image_jpeg,omitempty"`
+	Balance             uint            `json:"balance,omitempty"      bson:"balance,omitempty"`
+	Age                 int             `json:"age,omitempty"          bson:"-"`
+	Birthday            time.Time       `json:"birthday,omitempty"     bson:"birthday,omitempty"`
+	City                string          `json:"city,omitempty"         bson:"city,omitempty"`
+	Country             string          `json:"country,omitempty"      bson:"country,omitempty"`
+	Weight              uint            `json:"weight,omitempty"       bson:"weight,omitempty"`
+	Growth              uint            `json:"growth,omitempty"       bson:"growth,omitempty"`
+	Destinations        []string        `json:"destinations,omitempty" bson:"destinations,omitempty"`
+	Seasons             []string        `json:"seasons,omitempty"      bson:"seasons,omitempty"`
+	LastAction          time.Time       `json:"last_action,omitempty"  bson:"lastaction,omitempty"`
+	StatusUpdate        time.Time       `json:"-"                      bson:"statusupdate,omitempty"`
+	Favorites           []bson.ObjectId `json:"favorites,omitempty"    bson:"favorites,omitempty"`
+	Blacklist           []bson.ObjectId `json:"blacklist,omitempty"    bson:"blacklist,omitempty"`
+	Countries           []string        `json:"countries,omitempty"    bson:"countries,omitempty"`
+	LikingsSex          string          `json:"likings_sex,omitempty"          bson:"likings_sex,omitempty"`
+	LikingsDestinations []string        `json:"likings_destinations,omitempty" bson:"likings_destinations,omitempty"`
+	LikingsSeasons      []string        `json:"likings_seasons,omitempty"      bson:"likings_seasons,omitempty"`
 }
 
 func getHash(password, salt string) string {
@@ -95,6 +99,9 @@ func (u *User) CleanPrivate() {
 	u.Favorites = nil
 	u.Blacklist = nil
 	u.Balance = 0
+	u.LikingsDestinations = ""
+	u.LikingsSeasons = []string{}
+	u.LikingsDestinations = []string{}
 }
 
 func (u *User) SetAvatarUrl(adapter *weed.Adapter, db DataBase, webp WebpAccept) {
