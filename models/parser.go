7862dd9975a48c36b794e4843fe9acac4990a7f8
ernado <ernado@ya.ru>
2014-07-15 17:26:16 +0400

    field type

--- models/parser.go
+++ models/parser.go
@@ -19,7 +19,7 @@ func mapToStruct(q url.Values, val interface{}) error {
 	t := reflect.TypeOf(val)
 	for key, value := range q {
 		log.Printf("%s:%s", key, value)
-		field, _ := t.FieldByName(name)
+		field, _ := t.FieldByName(key)
 		if len(value) == 1 {
 			v := value[0]
 			vInt, err := strconv.Atoi(v)
