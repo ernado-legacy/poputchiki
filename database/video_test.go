2384cabab4f5d06c63e00d4fe14d7c3140d6e7f0
ernado <ernado@ya.ru>
2014-07-17 08:38:43 +0400

    video test

---
+++ database/video_test.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"github.com/ernado/poputchiki/models"
+	. "github.com/smartystreets/goconvey/convey"
+	"labix.org/v2/mgo/bson"
+	"testing"
+)
+
+func TestVideo(t *testing.T) {
+	db := TestDatabase()
+	Convey("Audio", t, func() {
+		Reset(func() {
+			db.Drop()
+		})
+		Convey("Add", func() {
+			a := &models.Video{}
+			a.Id = bson.NewObjectId()
+			a.User = bson.NewObjectId()
+			_, err := db.AddVideo(a)
+			So(err, ShouldBeNil)
+			Convey("Get", func() {
+				b := db.GetVideo(a.Id)
+				So(b.User, ShouldEqual, a.User)
+			})
+			Convey("Add like", func() {
+				So(db.AddLikeVideo(a.User, a.Id), ShouldBeNil)
+				Convey("Liked", func() {
+					c := db.GetVideo(a.Id)
+					So(c.Likes, ShouldEqual, 1)
+				})
+			})
+		})
+	})
+
+}
