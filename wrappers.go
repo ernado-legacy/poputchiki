28d1ab756bc79676172f801b58459b403e49d49a
ernado <ernado@ya.ru>
2014-08-07 20:39:04 +0400

    fixed build error

--- wrappers.go
+++ wrappers.go
@@ -166,7 +166,7 @@ func WebpWrapper(c martini.Context, r *http.Request) {
 }
 
 func VideoWrapper(c martini.Context, r *http.Request) {
-	var accept models.VideoAccept = AaAac
+	var accept models.VideoAccept = models.AaAac
 	cookie, err := r.Cookie("video")
 	if err != nil {
 		c.Map(accept)
@@ -179,7 +179,7 @@ func VideoWrapper(c martini.Context, r *http.Request) {
 }
 
 func AudioWrapper(c martini.Context, r *http.Request) {
-	var accept models.AudioAccept = VaWebm
+	var accept models.AudioAccept = models.VaWebm
 	cookie, err := r.Cookie("audio")
 	if err != nil {
 		c.Map(accept)
