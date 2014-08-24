2a810baaf81150b6a0a839c282b378cc197f9f33
ernado <ernado@ya.ru>
2014-08-24 18:09:00 +0400

    added regression test for audio add integrity

--- database/audio_test.go
+++ database/audio_test.go
@@ -12,19 +12,24 @@ func TestAudio(t *testing.T) {
 	db := TestDatabase()
 	Convey("Audio", t, func() {
 		Reset(db.Drop)
+		u := &models.User{Id: bson.NewObjectId(), Name: "Alex", Email: "test@kek.ru", Sex: models.SexMale}
+		So(db.Add(u), ShouldBeNil)
+		Convey("Integrity", Integrity(db, u))
 		Convey("Add", func() {
 			a := &models.Audio{}
 			a.Id = bson.NewObjectId()
-			a.User = bson.NewObjectId()
-			u := &models.User{}
-			u.Id = a.User
-			So(db.Add(u), ShouldBeNil)
+			a.User = u.Id
 			_, err := db.AddAudio(a)
 			So(err, ShouldBeNil)
 			Convey("Get", func() {
 				b := db.GetAudio(a.Id)
 				So(b.User, ShouldEqual, a.User)
 			})
+			Convey("User get", func() {
+				newUser := db.Get(u.Id)
+				So(newUser.Audio, ShouldEqual, a.Id)
+			})
+			Convey("Integrity", Integrity(db, u))
 			Convey("Update", func() {
 				fidAac := "1231424123"
 				fidOgg := "j980234089dsf"
@@ -34,6 +39,13 @@ func TestAudio(t *testing.T) {
 				So(b.User, ShouldEqual, a.User)
 				So(b.AudioAac, ShouldEqual, fidAac)
 				So(b.AudioOgg, ShouldEqual, fidOgg)
+				Convey("Integrity", Integrity(db, u))
+				Convey("User get", func() {
+					newUser := db.Get(u.Id)
+					So(newUser.Audio, ShouldEqual, a.Id)
+					So(newUser.AudioAAC, ShouldEqual, fidAac)
+					So(newUser.AudioOGG, ShouldEqual, fidOgg)
+				})
 			})
 		})
 		Convey("Update not found", func() {
