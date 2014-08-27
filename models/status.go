97b46703cdee77104256f9ab2e44051f3116dcee
ernado <ernado@ya.ru>
2014-08-27 17:13:27 +0400

    omitempty removed for text of status

--- models/status.go
+++ models/status.go
@@ -11,7 +11,7 @@ type Status struct {
 	User       bson.ObjectId   `json:"user,omitempty"   bson:"user"`
 	UserObject *User           `json:"user_object,omitempty" bson:"-"`
 	Time       time.Time       `json:"time,omitempty"   bson:"time"`
-	Text       string          `json:"text,omitempty"   bson:"text"`
+	Text       string          `json:"text"             bson:"text"`
 	ImageUrl   string          `json:"url,omitempty"    bson:"-"`
 	Likes      int             `json:"likes,omitempty"  bson:"likes"`
 	LikedUsers []bson.ObjectId `json:"liked_users"      bson:"liked_users"`
