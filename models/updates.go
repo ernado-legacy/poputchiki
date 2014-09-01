b2fd315520417adb65183986c6b999d0d0c1ca29
ernado <ernado@ya.ru>
2014-09-01 21:52:28 +0400

    updated letters

--- models/updates.go
+++ models/updates.go
@@ -58,9 +58,6 @@ func GetEventType(updateType string, media interface{}) string {
 	if updateType == SubscriptionInvites || updateType == SubscriptionMessages {
 		return updateType
 	}
-	log.Println(reflect.TypeOf(media))
-	log.Println(reflect.TypeOf(media).Name())
-	log.Println(media)
 	return fmt.Sprintf("%s_%s", updateType, strings.ToLower(reflect.TypeOf(media).Elem().Name()))
 }
 
