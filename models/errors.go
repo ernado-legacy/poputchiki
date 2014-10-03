23bb1ed47ad84f810957c668f6e8f9fd29a5b12b
ernado <ernado@ya.ru>
2014-10-03 18:30:20 +0400

    added errors module

---
+++ models/errors.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Error struct {
+	Code int    `json:"status"`
+	Text string `json:"error"`
+}
+
+type Response struct {
+	Status   int         `json:"status"`
+	Response interface{} `json:"response"`
+}
+
+var (
+	ErrorNotAllowed            = Error{http.StatusMethodNotAllowed, "You are trying to modify others user information"}
+	ErrorBlacklisted           = Error{http.StatusMethodNotAllowed, "You are blacklisted"}
+	ErrorAuth                  = Error{http.StatusUnauthorized, "Not authenticated"}
+	ErrorMarshal               = Error{http.StatusInternalServerError, "Unable to unmarshal data"}
+	ErrorUserNotFound          = Error{http.StatusNotFound, "User not found"}
+	ErrorObjectNotFound        = Error{http.StatusNotFound, "Object not found"}
+	ErrorBadId                 = Error{http.StatusBadRequest, "Bad user id"}
+	ErrorBadRequest            = Error{http.StatusBadRequest, "Bad request"}
+	ErrorInsufficentFunds      = Error{http.StatusPaymentRequired, "Insufficent funds"}
+	ErrorBackend               = Error{http.StatusInternalServerError, "Internal server error"}
+	ErrorUserAlreadyRegistered = Error{http.StatusBadRequest, "User already registered"}
+)
+
+func ValidationError(err error) Error {
+	return Error{http.StatusBadRequest, err.Error()}
+}
+
+func BackendError(err error) Error {
+	return Error{http.StatusInternalServerError, err.Error()}
+}
+
+func (e Error) Error() string {
+	return fmt.Sprintf("Error %d: %s", e.Code, e.Text)
+}
